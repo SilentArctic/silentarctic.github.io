e15bf586c31f526fbe78df2f5f3849098addba96
SilentArctic <silentarcticdev@gmail.com>
2022-10-28 17:50:00 -0400

    add crb, schemas; add action to generate collection index

---
+++ schemas/index.js
@@ -0,0 +1,24 @@
+/* eslint-disable camelcase */
+
+export { default as schema } from './schema.json';
+export { default as adventure_schema } from './adventure_schema.json';
+export { default as adversary_schema } from './adversary_schema.json';
+export { default as archetype_schema } from './archetype_schema.json';
+export { default as book_schema } from './book_schema.json';
+export { default as career_schema } from './career_schema.json';
+export { default as characteristic_schema } from './characteristic_schema.json';
+export { default as gear_schema } from './gear_schema.json';
+export { default as meta_schema } from './meta_schema.json';
+export { default as optionFeature_schema } from './optionFeature_schema.json';
+export { default as quality_schema } from './quality_schema.json';
+export { default as rule_schema } from './rule_schema.json';
+export { default as setting_schema } from './setting_schema.json';
+export { default as sidebar_schema } from './sidebar_schema.json';
+export { default as skill_schema } from './skill_schema.json';
+export { default as specialization_schema } from './specialization_schema.json';
+export { default as spell_schema } from './spell_schema.json';
+export { default as table_schema } from './table_schema.json';
+export { default as talent_schema } from './talent_schema.json';
+export { default as vehicle_schema } from './vehicle_schema.json';
+export { default as _settings_schema } from './_settings_schema.json';
+export { default as _varyingDisplay_schema } from './_varyingDisplay_schema.json';
